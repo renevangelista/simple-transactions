1a7f24ce285d2923378202553f53071bef045b1d
Renan Evangelista <renanmemories@gmail.com>
2021-06-10 17:23:45 -0300

    Instala Passport e cria APIs de registro e login de Users

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'prefix' => 'v1'
+], function () {
+    Route::post('register', 'Api\Auth\RegisterController@register')
+        ->name('user.register');
+    Route::post('login', 'Api\Auth\LoginController@login')
+        ->name('user.login');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
